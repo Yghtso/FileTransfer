e9f0483f0855c15c9090f7b4048705f2962bfb39
Cristian Soare <soare.cristian@outlook.com>
2024-11-26 11:23:50 +0100

    Aggiunto il main file

---
+++ src/main/java/scuola/esercitazione/filetransfer/FileTransfer.java
@@ -0,0 +1,16 @@
+package scuola.esercitazione.filetransfer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FileTransfer extends Application {
+    
+    @Override
+    public void start(Stage primarystage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
