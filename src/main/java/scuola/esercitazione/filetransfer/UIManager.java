b55dc72efa8a3f9da2130ef7c0d0874acf978a8c
Soare Cristian <soare.cristian@outlook.com>
2024-11-28 17:48:34 +0100

    Create le funzioni di render delle scene fxml

--- src/main/java/scuola/esercitazione/filetransfer/UIManager.java
+++ src/main/java/scuola/esercitazione/filetransfer/UIManager.java
@@ -1,6 +1,11 @@
 package scuola.esercitazione.filetransfer;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class UIManager {
@@ -42,15 +47,55 @@ public class UIManager {
 
     // Render delle diverse scene
     private void renderMainMenu() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainMenu.fxml"));
+        Parent root;
+
+        try {
+
+            root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
         
+        }
     }
 
     private void renderPuller() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Pusher.fxml"));
+        Parent root;
 
+        try {
+
+            root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        
+        }
     }
 
     private void renderPusher() {
 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Puller.fxml"));
+        Parent root;
+
+        try {
+
+            root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        
+        }
     }
 
 }
