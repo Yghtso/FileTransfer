c994fedbf775b7dd234b575aa4e0bc4b3552a45c
Soare Cristian <soare.cristian@outlook.com>
2024-11-28 15:55:26 +0100

    Righe base per render della schermata del menu principale

--- src/main/java/scuola/esercitazione/filetransfer/FileTransfer.java
+++ src/main/java/scuola/esercitazione/filetransfer/FileTransfer.java
@@ -1,13 +1,24 @@
 package scuola.esercitazione.filetransfer;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class FileTransfer extends Application {
     
     @Override
-    public void start(Stage primarystage) {
+    public void start(Stage primaryStage) throws Exception {
 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainMenu.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("FileTransfer");
+        primaryStage.show();
+        
     }
 
     public static void main(String[] args) {
